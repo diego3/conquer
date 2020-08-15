9da71b5c4fbbb0b718d14230b5dfb3e846988956
diego3 <www.diegosantos.com.br@gmail.com>
2020-08-14 22:10:47 -0300

    first commit

---
+++ src/main/java/com/diegorosa/conquer/ConquerApplication.java
@@ -0,0 +1,13 @@
+package com.diegorosa.conquer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConquerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConquerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegorosa/conquer/ConquerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegorosa.conquer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConquerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
