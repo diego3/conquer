37a9ffbeff3def371fad9bb38477af307de6dd54
diego3 <www.diegosantos.com.br@gmail.com>
2020-08-18 00:17:30 -0300

    frontend improvements, added a new menu to download csv reports

--- src/main/java/com/diegorosa/conquer/controller/DashboardController.java
+++ src/main/java/com/diegorosa/conquer/controller/DashboardController.java
@@ -1,11 +1,16 @@
 package com.diegorosa.conquer.controller;
 
+import com.diegorosa.conquer.service.ContratoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+
+    @Autowired
+    private ContratoService service;
 
     @GetMapping("/")
     public String relatorio(Model model) {
--- src/main/java/com/diegorosa/conquer/entity/Contrato.java
+++ src/main/java/com/diegorosa/conquer/entity/Contrato.java
@@ -2,7 +2,6 @@ package com.diegorosa.conquer.entity;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
 @Table(name = "contrato")
